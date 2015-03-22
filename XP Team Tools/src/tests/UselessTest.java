adea18389333363f879a465706e86df9de5429e5
Don Lele <donlelef@gmail.com>
2015-03-22 17:37:04 +0100

    Classes added: UselessTest.
    This is a test of GitHub connections.

---
+++ XP Team Tools/src/tests/UselessTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+/**
+ * Useless class: added with the only purpose of testing GitHub connection.
+ * @author lele
+ *
+ */
+public class UselessTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+	}
+
+}
