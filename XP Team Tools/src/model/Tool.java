399768c2b29b94f695344d288d22069d6978ea16
Don Lele <donlelef@gmail.com>
2015-03-23 23:14:16 +0100

    Modified class: Tool (added some features)

--- XP Team Tools/src/model/Tool.java
+++ XP Team Tools/src/model/Tool.java
@@ -1,10 +1,8 @@
-package tests;
+package model;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import model.Event;
-import model.Timeline;
 import boards.TasksManager;
 
 public class Tool {
@@ -14,23 +12,26 @@ public class Tool {
 
 	public void addTask(String taskName, String description) {
 		this.taskManager.addTask(taskName, description);
-		String creationDate = getCurrentDate();
-		this.timeline.addEvent(new Event("Created task: " + taskName, creationDate));
+		this.timeline.addEvent(new Event("Created task: " + taskName, getCurrentDate()));
 	}
 
 	public int getEventsNumber() {
 		return this.timeline.getEventsNumber();
 	}
-	
+
+	public void deleteTask(String taskName) {
+		this.taskManager.deleteTask(taskName);
+		timeline.addEvent(new Event("Deleted task: "+ taskName, getCurrentDate()));
+	}
+
+	public Event getEvent(String eventName) {
+		return this.timeline.getEvent(eventName);
+	}
+
 	private String getCurrentDate() {
 		SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
 		Calendar cal = Calendar.getInstance();
 		String creationDate = format.format(cal.getTime());
 		return creationDate;
 	}
-	
-	
-	
-	
-
 }
--- XP Team Tools/src/tests/TimelineIntegrationTest.java
+++ XP Team Tools/src/tests/TimelineIntegrationTest.java
@@ -1,17 +1,48 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import model.Tool;
 
 import org.junit.Test;
 
 public class TimelineIntegrationTest {
 
 	Tool tool = new Tool();
-	
+
 	@Test
-	public void test() {
+	public void taskAdditionCreatesEvent() {
 		tool.addTask("Nuovo task", "Integrare task in timeline");
-		assertEquals(2, tool.getEventsNumber());		
+		assertEquals(2, tool.getEventsNumber());
+	}
+
+	@Test
+	public void taskDeletionCreatesEvent() {
+		tool.addTask("Timer", "creare un timer che...");
+		tool.addTask("Timeline", "creare una classe che...");
+		tool.deleteTask("Timer");
+		assertEquals(4, tool.getEventsNumber());
+	}
+
+	@Test
+	public void automaticEventsTest() throws Exception {
+		String currentDate = getCurrentDate();
+		tool.addTask("Timer", "");
+		assertEquals("Created task: Timer",
+				tool.getEvent("Created task: Timer").toString());
+		assertEquals(currentDate,
+				tool.getEvent("Created task: Timer").getDate());
+		
+	}
+
+	private String getCurrentDate() {
+		SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
+		Calendar cal = Calendar.getInstance();
+		String currentDate = format.format(cal.getTime());
+		return currentDate;
 	}
 
 }
