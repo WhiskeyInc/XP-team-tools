cf0c4b8b7c39ec4ee03e5a011e9e69ebf10dcecd
alessandrobardini <alessandro.bardini93@gmail.com>
2015-03-26 20:55:11 +0100

    Added the test of the Timer UI

---
+++ XP Team Tools/src/timer/tests/TimerTest02.java
@@ -0,0 +1,21 @@
+package timer.tests;
+
+import javax.swing.JFrame;
+
+import ui.TimerUI;
+import ui.tests.FramesUtils;
+
+public class TimerTest02 {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = FramesUtils.createFrame("Timer", 200, 200);
+		TimerUI timer = new TimerUI();
+		frame.getContentPane().add(timer);
+		timer.setSecond(3);
+		timer.setMinute(1);
+		frame.setVisible(true);
+	}
+	
+
+}
