ead425801132017922a9e8adfe8408d1273525ee
Don Lele <donlelef@gmail.com>
2015-03-30 00:44:58 +0200

    Modified classes: DrawableTimeline (it draws a row to represent the timeline and the events)

--- XP Team Tools/src/gui/GUITest02.java
+++ XP Team Tools/src/gui/GUITest02.java
@@ -14,16 +14,11 @@ public class GUITest02 {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("GUI Test02");
 		Timeline timeline = new Timeline();
-		timeline.addEvent(new Event("Riunione", new GregorianCalendar(2015, 02, 30, 23, 44, 04)));
-//		timeline.addEvent(new Event("Riunione2", (GregorianCalendar) Calendar
-//				.getInstance()));
-//		timeline.addEvent(new Event("Riunione3", (GregorianCalendar) Calendar
-//				.getInstance()));
-//		timeline.addEvent(new Event("Riunione nella tana del topo",
-//				(GregorianCalendar) Calendar.getInstance()));
-//		timeline.addEvent(new Event(
-//				"sacjbd.kfvbdof ghklvjbfdkghskvjbfkksjbvkdfkdjbvkjdvkfjvbkdjvhfdkvbjdk",
-//				(GregorianCalendar) Calendar.getInstance()));
+		timeline.addEvent(new Event("Riunione", new GregorianCalendar(2015, 03, 29, 23, 44, 04)));
+		timeline.addEvent(new Event("Riunione1", new GregorianCalendar(2015, 02, 30, 23, 44, 04)));
+		timeline.addEvent(new Event("Riunione2", new GregorianCalendar(2015, 03, 3, 23, 44, 04)));
+		timeline.addEvent(new Event("Riunione3", new GregorianCalendar(2015, 03, 6, 23, 44, 04)));
+		timeline.addEvent(new Event("Riunione4", new GregorianCalendar(2015, 04, 25, 23, 44, 04)));
 		Drawer drawer = new Drawer(new DrawableTimeline(timeline));
 		frame.getContentPane().add(drawer);
 		frame.pack();
--- XP Team Tools/src/gui/drawables/DrawableTimeline.java
+++ XP Team Tools/src/gui/drawables/DrawableTimeline.java
@@ -4,15 +4,21 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.GregorianCalendar;
+import java.util.Random;
 
 import timeline.Event;
 import timeline.Timeline;
 import filtering.NoFilter;
 
 public class DrawableTimeline implements Drawable {
-	
-	private static final int MARGIN = 10;
-	private static final int SECONDS_PER_PIXEL = 605; //A week is approximately 1000 px
+
+	private static final int EVENTBOX_WIDTH = 120;
+	private static final int EVENTBOX_HEIGHT = 60;
+	private static final int ROW_Y_MIN_MARGIN = 30;
+	private static final int ROW_Y_MAX_MARGIN = 490;
+	private static final int MARGIN = 10 + EVENTBOX_WIDTH / 2;
+	private static final int SECONDS_PER_PIXEL = 605; // A week is approximately
+														// 1000 px
 	private static final float ROWY_PERCENTAGE = (float) 0.8;
 	private Timeline timeline;
 
@@ -20,23 +26,40 @@ public class DrawableTimeline implements Drawable {
 		super();
 		this.timeline = timeline;
 	}
-	
+
 	@Override
 	public void draw(Graphics g, int x, int y, int width, int height) {
-		
+
 		int timelineSeconds = getTimelineSeconds();
 		int initialX = x + MARGIN;
-		int rowY = y + (int) (ROWY_PERCENTAGE*height);
-		g.drawLine(initialX, rowY, initialX + timelineSeconds/SECONDS_PER_PIXEL, rowY);
-		
-		
-		
+		int rowY = y + (int) (ROWY_PERCENTAGE * height);
+		g.drawLine(initialX, rowY, initialX + timelineSeconds
+				/ SECONDS_PER_PIXEL, rowY);
 
-//		for (Event event : timeline.getEvents(new NoFilter<Event>())) {
-//			DrawableEvent drawableEvent = new DrawableEvent(event);
-//			drawableEvent.draw(g, x, y, width, height);
-//			x = x + 2*width;
-//		}
+		for (Event event : timeline.getEvents(new NoFilter<Event>())) {
+			DrawableEvent drawableEvent = new DrawableEvent(event);
+			int lineY = getY(rowY);
+			drawableEvent.draw(g, getEventX(initialX, event), lineY - EVENTBOX_HEIGHT,
+					EVENTBOX_WIDTH, EVENTBOX_HEIGHT);
+			g.drawLine(getLineX(initialX, event), lineY,
+					getLineX(initialX, event), rowY);
+
+		}
+	}
+
+	private int getY(int rowY) {
+		Random randomGenerator = new Random();
+		return rowY - ROW_Y_MIN_MARGIN
+				- randomGenerator.nextInt(ROW_Y_MAX_MARGIN - ROW_Y_MIN_MARGIN);
+	}
+
+	private int getLineX(int initialX, Event event) {
+		return initialX + timelineSeconds(event, timeline.getEvent("creation"))
+				/ SECONDS_PER_PIXEL;
+	}
+
+	private int getEventX(int initialX, Event event) {
+		return getLineX(initialX, event) - EVENTBOX_WIDTH / 2;
 	}
 
 	private int getTimelineSeconds() {
@@ -49,8 +72,8 @@ public class DrawableTimeline implements Drawable {
 	private int timelineSeconds(Event lastEvent, Event initialEvent) {
 		GregorianCalendar lastDate = lastEvent.getDate();
 		GregorianCalendar initialDate = initialEvent.getDate();
-		int secondsOfFirstEvent = (int) (initialDate.getTimeInMillis()/1000);
-		int secondsToLastEvent = (int) (lastDate.getTimeInMillis()/1000);
+		int secondsOfFirstEvent = (int) (initialDate.getTimeInMillis() / 1000);
+		int secondsToLastEvent = (int) (lastDate.getTimeInMillis() / 1000);
 		int timelineSeconds = secondsToLastEvent - secondsOfFirstEvent;
 		return timelineSeconds;
 	}
@@ -58,7 +81,7 @@ public class DrawableTimeline implements Drawable {
 	private Event getLastEvent() {
 		ArrayList<Event> list = timeline.getEvents(new NoFilter<Event>());
 		Collections.sort(list);
-		Event lastEvent = list.get(list.size()-1);
+		Event lastEvent = list.get(list.size() - 1);
 		return lastEvent;
 	}
 
