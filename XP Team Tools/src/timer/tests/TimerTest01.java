7ad58de881798b9fdd09c9640bb8c62f56a4a62a
alessandrobardini <alessandro.bardini93@gmail.com>
2015-04-01 18:24:29 +0200

    Modified TimerTest (because of the introduction of 2 new Classes)

--- XP Team Tools/src/timer/tests/TimerTest01.java
+++ XP Team Tools/src/timer/tests/TimerTest01.java
@@ -3,6 +3,8 @@ package timer.tests;
 import javax.swing.JFrame;
 
 import sounds.SoundPlayer;
+import timer.utils.TimerIndexManager;
+import timer.utils.TimerManager;
 import ui.TimerUI;
 import ui.tests.FramesUtils;
 
@@ -12,11 +14,14 @@ public class TimerTest01 {
 		
 		JFrame frame = FramesUtils.createFrame("Timer", 500, 200);
 		TimerUI timer = new TimerUI();
+		TimerManager manager = new TimerManager();
 		frame.getContentPane().add(timer);
-		timer.setSecond(5);
-		timer.setMinute(0);
-		timer.setPauseMinute(0);
-		timer.setPauseSecond(3);
+		manager.setInitialMinute(0);
+		manager.setInitialSecond(5);
+		manager.setPauseMinute(0);
+		manager.setPauseSecond(3);
+		timer.setManager(manager);
+		timer.setIndexManager(new TimerIndexManager(manager));
 		SoundPlayer player = new SoundPlayer("sounds/cannon.wav");
 		timer.setPlayer(player);
 		frame.setVisible(true);
