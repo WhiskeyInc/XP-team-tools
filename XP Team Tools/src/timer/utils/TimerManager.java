4d3c3261e785df42153baab222ba8e7c9ee791b8
alessandrobardini <alessandro.bardini93@gmail.com>
2015-04-01 18:21:38 +0200

    Created 2 new Classes (TimerManager and TimerIndexManager), extracted from TimerUI because of code smells.

---
+++ XP Team Tools/src/timer/utils/TimerManager.java
@@ -0,0 +1,71 @@
+package timer.utils;
+
+public class TimerManager {
+	
+	private int second;
+	private int initialSecond;
+	private int minute;
+	private int initialMinute;
+	private int pauseSecond;
+	private int pauseMinute;
+	private String display;
+	
+	public String getDisplay() {
+		if (minute < 10 && second < 10)
+			display = "0" + String.valueOf(minute) + " : 0"
+					+ String.valueOf(second);
+		if (minute >= 10 && second < 10)
+			display = String.valueOf(minute) + " : 0" + String.valueOf(second);
+		if (minute < 10 && second >= 10)
+			display = "0" + String.valueOf(minute) + " : "
+					+ String.valueOf(second);
+		if (minute >= 10 && second >= 10)
+			display = String.valueOf(minute) + " : " + String.valueOf(second);
+		return display;
+	}
+	
+	public void minuteTerminatedActions(){
+		second = 60;
+		minute--;
+	}
+	
+	public void secondDecrementer(){
+		second--;
+	}
+
+	public void setTimer(){
+		second = initialSecond + 1;
+		minute = initialMinute;
+	}
+	
+	public void setPause(){
+		second = pauseSecond;
+		minute = pauseMinute;
+	}
+	
+	public void setInitialSecond(int initialSecond) {
+		this.initialSecond = initialSecond;
+		second = initialSecond;
+	}
+	
+	public void setInitialMinute(int initialMinute) {
+		this.initialMinute = initialMinute;
+		minute = initialMinute;
+	}
+
+	public void setPauseMinute(int pauseMinute) {
+		this.pauseMinute = pauseMinute;
+	}
+
+	public void setPauseSecond(int pauseSecond) {
+		this.pauseSecond = pauseSecond;
+	}
+	
+	public int getSecond() {
+		return second;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+}
