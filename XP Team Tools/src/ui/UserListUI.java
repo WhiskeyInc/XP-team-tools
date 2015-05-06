f6bc91ce69c064b963715cee3ecaa747d6b2b356
alessandrobardini <alessandro.bardini93@gmail.com>
2015-05-06 17:34:39 +0200

    Modified the logic of UserListUI (now, JLabels replace CheckBoxes)

--- XP Team Tools/src/ui/UserListUI.java
+++ XP Team Tools/src/ui/UserListUI.java
@@ -1,19 +1,20 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -23,7 +24,7 @@ public class UserListUI extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	private ArrayList<JCheckBox> box = new ArrayList<JCheckBox>();
+	private ArrayList<JLabel> labels = new ArrayList<JLabel>();
 	private String[] nicknames;
 	private JPanel nicksPanel = new JPanel();
 	private JButton button = new JButton("Start new chat");
@@ -35,12 +36,14 @@ public class UserListUI extends JPanel {
 		super.setLayout(new GridBagLayout());
 		super.setPreferredSize(new Dimension(250, 400));
 		super.setMinimumSize(new Dimension(250, 400));
-		super.setBorder(BorderFactory.createCompoundBorder(BorderFactory
-				.createTitledBorder(null, "Team Members panel",
-						TitledBorder.DEFAULT_JUSTIFICATION,
-						TitledBorder.DEFAULT_POSITION, new java.awt.Font(
-								"Verdana", 1, 8)), BorderFactory
-				.createEmptyBorder(1, 1, 1, 1)));
+		super.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(
+		         null, "Team Members panel",
+		         TitledBorder.DEFAULT_JUSTIFICATION,
+		         TitledBorder.DEFAULT_POSITION,
+		         new java.awt.Font("Verdana", 1, 8)
+		      ),
+		      BorderFactory.createEmptyBorder(1, 1, 1, 1)
+		   ));
 		GridBagConstraints lim = new GridBagConstraints();
 		lim.gridx = 0;
 		lim.gridy = 0;
@@ -50,19 +53,20 @@ public class UserListUI extends JPanel {
 		super.add(label, lim);
 
 	}
+	
+	
 
 	public UserListUI(String[] nicknames) {
 		super();
 		this.nicknames = nicknames;
 	}
 
-	public void setButtonAction(ActionListener listener) {
+
+
+	public void setButtonAction(ActionListener listener){
 		button.addActionListener(listener);
 	}
-
-	public ArrayList<JCheckBox> getBox() {
-		return box;
-	}
+	
 
 	public String[] getNicknames() {
 		return nicknames;
@@ -71,7 +75,7 @@ public class UserListUI extends JPanel {
 	public ArrayList<String> getListOfSelectedNicknames() {
 		return selectedNicknames;
 	}
-
+	
 	public String[] getSelectedNicknames() {
 		int size = selectedNicknames.size();
 		String[] sel = new String[size];
@@ -80,42 +84,72 @@ public class UserListUI extends JPanel {
 		}
 		return sel;
 	}
-
-	// TODO javadoc
+	//TODO javadoc
 	public void setNicknames(String[] nicknames) {
 		this.nicknames = nicknames;
 		int size = nicknames.length;
 		nicksPanel.removeAll();
-		nicksPanel.setLayout(new GridLayout(size, 1));
-		// Spostare su un altro pannello...
-		// super.setLayout(new GridBagLayout());
-		// GridBagConstraints lim = new GridBagConstraints();
-		// lim.gridx = 0;
-		// lim.gridy = 0;
-		// JLabel label = new JLabel("Team members");
-		// label.setFont(new Font("TimesRoman", Font.BOLD, 16));
-		// lim.insets = new Insets(5, 5, 5, 5);
-		// super.add(label, lim);
-		// // fino a qua
-		box.clear();
-		for (int i = 0; i < size; i++) {
-			box.add(new JCheckBox(nicknames[i]));
-			final int index = i;
-			box.get(i).addActionListener(new ActionListener() {
+		nicksPanel.setLayout(new GridLayout(size*2 + 1,1));
+		//Spostare su un altro pannello...
+//		super.setLayout(new GridBagLayout());
+//		GridBagConstraints lim = new GridBagConstraints();
+//		lim.gridx = 0;
+//		lim.gridy = 0;
+//		JLabel label = new JLabel("Team members");
+//		label.setFont(new Font("TimesRoman", Font.BOLD, 16));
+//		lim.insets = new Insets(5, 5, 5, 5);
+//		super.add(label, lim);
+//		// fino a qua
+		nicksPanel.add(new JLabel());
+		for(int index = 0; index< size;index++){
+			final int i = index;
+			labels.add(new JLabel(nicknames[i]));
+			labels.get(i).addMouseListener(new MouseListener() {
+				
 				@Override
-				public void actionPerformed(ActionEvent e) {
-					if (box.get(index).isSelected()) {
-						box.get(index).setForeground(Color.GREEN);
-					} else {
-						box.get(index).setForeground(Color.BLACK);
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent e) {
+					Cursor cursor = Cursor.getDefaultCursor();
+				    cursor = Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR); 
+				    setCursor(cursor);
+					
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					Cursor cursor = Cursor.getDefaultCursor();
+				    cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR); 
+				    setCursor(cursor);
+					
+				}
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if(labels.get(i).getForeground().equals(Color.BLUE)){
+						labels.get(i).setForeground(Color.BLACK);
+					}else{
+					labels.get(i).setForeground(Color.BLUE);
 					}
 				}
 			});
-			nicksPanel.add(box.get(i));
+			nicksPanel.add(labels.get(index));
+			nicksPanel.add(new JLabel());
 		}
+		
 		JScrollPane pane = new JScrollPane(nicksPanel);
-		// pane.setPreferredSize(new Dimension(170, 300));
-		// pane.setMinimumSize(new Dimension(170, 300));
+//		pane.setPreferredSize(new Dimension(170, 300));
+//		pane.setMinimumSize(new Dimension(170, 300));
 		GridBagConstraints lim = new GridBagConstraints();
 		lim.gridx = 0;
 		lim.gridy = 1;
@@ -130,18 +164,16 @@ public class UserListUI extends JPanel {
 		button.setFont(new Font("TimesRoman", Font.BOLD, 10));
 		button.setPreferredSize(new Dimension(170, 40));
 		button.setMinimumSize(new Dimension(170, 40));
+		
 		super.add(button, lim);
 	}
-
+	
 	public JButton getButton() {
 		return button;
 	}
 	
-	public void deselectAll() {
-		for (JCheckBox jCheckBox : box) {
-			jCheckBox.setSelected(false);
-			jCheckBox.setForeground(Color.BLACK);
-		}
+	public ArrayList<JLabel> getLabels() {
+		return labels;
 	}
 
 }
