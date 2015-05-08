53d19d5636dc7fbb6985a0b79e63fa9736b8ec95
alessandrobardini <alessandro.bardini93@gmail.com>
2015-05-08 11:43:34 +0200

    Added the Loading Panel to MainUI

--- XP Team Tools/src/ui/MainUIObserver.java
+++ XP Team Tools/src/ui/MainUIObserver.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -17,6 +18,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 import javax.swing.border.TitledBorder;
 
 import protocol.JsonMaker;
@@ -44,6 +46,8 @@ public class MainUIObserver extends JFrame implements Observer {
 	private SetMembsService setTeamMembs;
 	private JPanel mainPanel;
 	private StrategyClient1_1 client;
+	private LoadingPanel lp = new LoadingPanel("Loading");
+	private JPanel loadingPanel = new JPanel();
 
 	// setMessage = 0
 	// setTimeStamp = 1
@@ -196,4 +200,28 @@ public class MainUIObserver extends JFrame implements Observer {
 		refresh();
 	}
 
+	public void insertLoadingPanel(){
+		loadingPanel.add(lp);
+		Timer timer1 = new Timer(1000, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(lp.getLabelText().contains("...")){
+					lp.setLabelText("Loading");
+				}
+				lp.setLabelText(lp.getLabelText()+".");
+				getContentPane().add(loadingPanel);
+				setVisible(true);
+			}
+		});
+		
+		timer1.start();
+		
+		setVisible(true);
+	}
+	
+	public void removeLoadingPanel(){
+		getContentPane().add(mainPanel);
+		setVisible(true);
+	}
 }
--- XP Team Tools/src/ui/UIObserverStrategy1.java
+++ XP Team Tools/src/ui/UIObserverStrategy1.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -11,6 +12,7 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 import client.model.IClientService;
 import client.model.MessageObservable;
@@ -30,7 +32,10 @@ public class UIObserverStrategy1 extends JFrame {
 
 	private ChatUIObserverStrategy1 chatUI;
 	private TimerUIObserverStrategy timerUI;
-
+	private LoadingPanel lp = new LoadingPanel("Loading");
+	private JPanel panel = new JPanel();
+	private JPanel loadingPanel = new JPanel();
+	
 	public UIObserverStrategy1(final IClientService setMessage,
 			final IClientService setTimeStamp, final StrategyClient1_1 client,
 			 int index) {
@@ -45,9 +50,7 @@ public class UIObserverStrategy1 extends JFrame {
 
 			@Override
 			public void run() {
-			
-
-				JPanel panel = new JPanel();
+		
 				panel.setLayout(new GridBagLayout());
 				GridBagConstraints lim = new GridBagConstraints();
 				lim.gridx = 0;
@@ -99,5 +102,32 @@ public class UIObserverStrategy1 extends JFrame {
 	public ChatUIObserverStrategy1 getChatUI() {
 		return chatUI;
 	}
+	
+	public void insertLoadingPanel(){
+		loadingPanel.add(lp);
+		Timer timer1 = new Timer(1000, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(lp.getLabelText().contains("...")){
+					lp.setLabelText("Loading");
+				}
+				lp.setLabelText(lp.getLabelText()+".");
+				getContentPane().add(loadingPanel);
+				setVisible(true);
+			}
+		});
+		
+		timer1.start();
+		
+		setVisible(true);
+	}
+	
+	public void removeLoadingPanel(){
+		getContentPane().add(panel);
+		setVisible(true);
+	}
+	
+	
 
 }
