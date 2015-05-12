373333a900001898c80cfbc142d18393496ef31f
Pavlo <p_koelio@hotmail.com>
2015-05-12 12:10:44 +0200

    Local Session Handling added;

--- XP Team Tools/src/ui/login/RememberMeListener.java
+++ XP Team Tools/src/ui/login/RememberMeListener.java
@@ -2,24 +2,35 @@ package ui.login;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 import javax.swing.JCheckBox;
+
+import server.utils.ISessionSaver;
 
 public class RememberMeListener implements ItemListener {
 
 	private JCheckBox saveCheck;
-	//private SessionSaver sessionSaver = new SessionSaver();
+	private ISessionSaver sessionSaver;
 	
-	public RememberMeListener(JCheckBox saveCheck) {
+	public RememberMeListener(ISessionSaver sessionSaver, JCheckBox saveCheck) {
 		this.saveCheck = saveCheck;
+		this.sessionSaver = sessionSaver;
 	}
 	
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		Object source = e.getItemSelectable();
 
-		if (source == saveCheck) {
-			//sessionsSaver.saveSession();
+		if (saveCheck.isSelected()) {
+			try {
+				sessionSaver.saveSession();
+				System.out.println("Ciaooo");
+			} catch (NoSuchAlgorithmException | IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		} else {
 			//sessionSaver.deleteSession();
 		}
