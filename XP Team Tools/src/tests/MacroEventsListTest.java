afd21ec0014d45af60ae1f5b76d23308515ae3a4
Nicola Latella <NicLat@users.noreply.github.com>
2015-05-18 22:05:35 +0200

    Added test for the macro events communication

---
+++ XP Team Tools/src/tests/MacroEventsListTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import protocol.JsonMaker;
+import protocol.JsonParser;
+import client.model.MacroEvents;
+import events.SendPost;
+
+public class MacroEventsListTest {
+	
+	public static void main(String[] args) {
+		
+
+		SendPost sender = new SendPost("http://xtream-whiskeyinc.rhcloud.com/XPTT_Web/JSONAcceptor");
+		String message = JsonMaker.requestMacroEventsList("admin");
+					
+		String answer = sender.sendJson(message);
+		
+		//TODO test the request for the events to the other server
+		//TODO remove later fake answer:
+		//answer = "{\"request\": \"14\",\"action\": \"macro_event_response\",\"user\": \"admin\",\"ids\": [\"001\",\"002\",\"003\"],\"names\": [\"Incre programma tutto il dì\",\"Ciao LELE\",\"Martin fera\"]}";
+		
+		final MacroEvents events = JsonParser.parseMacroEventsResponse(answer);
+		
+		System.out.println("Macro events: ");
+		for (String string : events.getNames()) {
+			System.out.println(string);
+		}
+		
+	}
+
+}
