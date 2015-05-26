11c165d151d3d6fd9fb82bee3dfd2890e5c61b05
Emanuele Fabbiani <donlelef@gmail.com>
2015-05-26 12:03:48 +0200

    Closed package: control.actions.userStories
    Small refactor and documentation added.

--- XPTT_Web/src/control/actions/userStories/USAdder.java
+++ XPTT_Web/src/control/actions/userStories/USAdder.java
@@ -6,21 +6,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import control.HttpAction;
 import model.exceptions.NameAlreadyInUseException;
 import model.project.Project;
 import boards.UserStoryBoard.UserStory;
 import boards.taskBoard.ConcreteTaskManager;
+import control.HttpAction;
 
+/**
+ * This class adds a user story to the board of the project the user is
+ * visualizing. To work properly, this class requires that the following
+ * attributes are properly set in the request: title, description.
+ * 
+ * @author lele, simo, incre, andre
+ * @see {@link HttpAction}, {@link UserStory}
+ *
+ */
 public class USAdder implements HttpAction {
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see control.HttpAction#perform(javax.servlet.http.HttpServletRequest,
+	 * javax.servlet.http.HttpServletResponse)
+	 */
 	public void perform(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Project project = (Project) request.getSession().getAttribute("currentProject");
+		Project project = (Project) request.getSession().getAttribute(
+				"currentProject");
 		String title = request.getParameter("title");
 		String description = request.getParameter("description");
-		UserStory story = new UserStory(title, description, new ConcreteTaskManager());
+		UserStory story = new UserStory(title, description,
+				new ConcreteTaskManager());
 		try {
 			project.getUserStoriesManager().addUserStory(story);
 		} catch (NameAlreadyInUseException e) {
